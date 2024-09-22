24899a8dcf6711926fb3cb6ed2fdbfcbb9b85f39
prasenbhanari <prasenbhanari2003@gmail.com>
2024-09-22 17:45:41 +0530

    show hidden files

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,6 +7,13 @@ return {
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
 	opts = {
+		filesystem = {
+			filtered_items = {
+				--visible = true,
+				hide_dotfiles = false,
+			},
+		},
+
 		window = {
 			mappings = {
 				["<Tab>"] = "open",
